70992e948c525f8717bc7624702650d60eb5b4d8
TimoKats <tpakats@gmail.com>
2025-08-31 17:02:54 +0200

    add funding, make installable

---
+++ emmer.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	server "github.com/TimoKats/emmer/server"
+)
+
+func main() {
+	// basics
+	fs := http.FileServer(http.Dir("web/static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// api
+	http.HandleFunc("/api/ping", server.PingHandler)
+	http.HandleFunc("/api/{item}/add", server.AddHandler)
+	http.HandleFunc("/api/{item}/del", server.DelHandler)
+	http.HandleFunc("/api/{item}/query", server.QueryHandler)
+
+	// start the server
+	log.Println("server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
