7d13da0e4a73b5ab712a6ef3f43ef2cdbd1f1fc2
TimoKats <tpakats@gmail.com>
2026-02-15 15:12:43 +0100

    checkpoint. create csv submodule

--- server/fs/json.go
+++ server/fs/json.go
@@ -6,7 +6,6 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
-	"runtime"
 	"strings"
 )
 
@@ -17,26 +16,6 @@ type Json struct {
 // takes filename and returns full path + extension to json file
 func (fs Json) getPath(filename string) string {
 	return filepath.Join(fs.Folder, filename) + ".json"
-}
-
-// selects folder based on OS and env variables of the user
-func selectFolder() string {
-	// user select
-	if folder := os.Getenv("EM_FOLDER"); folder != "" {
-		return folder
-	}
-	// defaults
-	if runtime.GOOS == "windows" {
-		// Use %AppData% on Windows
-		return filepath.Join(os.Getenv("AppData"), "emmer")
-	} else {
-		// Use XDG_DATA_HOME on linux (if exists)
-		xdgData := os.Getenv("XDG_DATA_HOME")
-		if xdgData == "" {
-			return filepath.Join(os.Getenv("HOME"), ".local", "share", "emmer")
-		}
-		return filepath.Join(xdgData, "emmer")
-	}
 }
 
 // creates empty (or prefilled) JSON file at path
